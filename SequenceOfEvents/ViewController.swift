9f807a42397989158e29edd436732c462a6eb157
Aleksander Shigin <a.sh.10031987@gmail.com>
2019-04-27 21:37:26 +0300

    Initial Commit

---
+++ SequenceOfEvents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SequenceOfEvents
+//
+//  Created by Alexander on 27/04/2019.
+//  Copyright © 2019 Alexander Shigin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
