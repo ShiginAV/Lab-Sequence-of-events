05b7893cbab066ba9f41659be238fab8be113a3d
Aleksander Shigin <a.sh.10031987@gmail.com>
2019-04-28 13:07:52 +0300

    first commit

--- SequenceOfEvents/ViewController.swift
+++ SequenceOfEvents/ViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
+var infoString = ""
 class ViewController: UIViewController {
-
+    @IBOutlet weak var infoLabel: UILabel?
+    var isViewDidLoad = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        isViewDidLoad = true
+        infoString = ""
+        self.addInfo(#function)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+  
+        if !isViewDidLoad {
+            infoString = ""
+        }
+        self.addInfo(#function)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+     
+        self.addInfo(#function)
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        self.addInfo(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        isViewDidLoad = false
+        self.addInfo(#function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+     
+        self.addInfo(#function)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    
+        self.addInfo(#function)
+    }
+    
+    func addInfo(_ function: String) {
+        infoString += "\(self.title ?? "no title") - \(function)\n"
+        if let infoLabel = infoLabel {
+            infoLabel.text = infoString
+        }
+    }
 }
 
